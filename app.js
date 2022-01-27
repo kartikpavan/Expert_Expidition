3e9c06508b9805e36b69d3793599cef15349a40f
kartikpavan <kartik.varanasi@yahoo.com>
2022-01-27 19:26:41 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use("/", userRouter);
 //   next();
 // });
 
-app.listen(3000, () => {
-  console.log("Serving on localhost:3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving at ${port}`);
 });
